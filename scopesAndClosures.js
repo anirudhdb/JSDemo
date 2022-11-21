c53e776b0afbb4c74e14aa4edcec73c6c13b98fc
Anirudh Bhargav <anirudhdb@gmail.com>
2022-11-21 16:10:08 +0530

    Exercise

---
+++ scopesAndClosures.js
@@ -0,0 +1,14 @@
+var a = 10;
+function outer(){
+    var b = a;
+    console.log(b);
+    function inner(){
+        var c = b;
+        console.log(c);
+        var b = 20;
+    }
+    inner();
+}
+
+outer()
+
