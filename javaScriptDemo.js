3492e76bd60bc8facf28b5083410ae5b5fda5494
Anirudh Bhargav <anirudhdb@gmail.com>
2022-11-21 11:03:41 +0530

    Demo of array declaration, array methods

--- javaScriptDemo.js
+++ javaScriptDemo.js
@@ -22,3 +22,17 @@ if(myState === obj.address.state)
     console.log("States are equal");
 }
 
+var myArray = [10,20,30,"Hello",{}];
+myArray.push(1000);
+console.log(myArray)
+myArray.shift()                                        //array methods
+console.log(myArray)
+myArray.pop()
+console.log(myArray)
+myArray.unshift("new")
+console.log(myArray)
+
+
+
+
+
