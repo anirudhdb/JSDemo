158829bfefcce62bc3681d24c98f287dce93b6be
Anirudh Bhargav <anirudhdb@gmail.com>
2022-11-21 10:51:06 +0530

    Nested Objects

--- javaScriptDemo.js
+++ javaScriptDemo.js
@@ -3,3 +3,17 @@
     var b = 20;
     console.log(a+b);    //IIFE
 })();
+
+
+var obj = {
+    "name" : "abc",
+    "age" : 55,
+    "address" : {
+        "street" : "JS street",
+        "city" : "Bangalore",
+        "state" : "Karnataka"
+    }
+}
+
+console.log(obj);
+
