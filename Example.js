dc051c213ef41953e02bf2fcde3802261591158f
Anirudh Bhargav <anirudhdb@gmail.com>
2022-11-22 11:52:32 +0530

    Demo of setter methods

--- Example.js
+++ Example.js
@@ -8,10 +8,19 @@ function createPerson(){
         },
         "getLastName" : function(){
                 return lastName;
-            }
+        },
+        "setFirstName" : function(fname){
+            firstName = fname;
+        },
+        "setLastName" : function(lname){
+            lastName = lname;
+        }
         };
         return returnObj;
     }
 
     var person = createPerson();
     console.log(person.getFirstName() +" "+ person.getLastName());
+    person.setFirstName("Foo");
+    person.setLastName("FIFA");
+    console.log(person.getFirstName() + " "+person.getLastName());
